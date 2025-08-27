973ddfd63bd5104887337f13f735a566ade201f2
Dawid Pionk <150350371+DawidP2001@users.noreply.github.com>
2025-08-27 15:20:00 +0100

    Began work on cloud sprites and background class

---
+++ Background.h
@@ -0,0 +1,13 @@
+#ifndef BACKGROUND_H
+#define BACKGROUND_H
+
+class Background {
+private:
+	sf::Texture skyTexture;
+	sf::Texture grassTexture;
+	std::vector<sf::Texture> clouds;
+public:
+	Background() :
+
+};
+#endif
--- Floppy-Dog.cpp
+++ Floppy-Dog.cpp
@@ -6,6 +6,7 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "Background.h"
 
 class Player {
 private:
